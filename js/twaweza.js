eb708c968f841ba6b254874cffcd70e765f61e48
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-11-28 06:20:27 +0300

    Add open and close navigation functions

--- js/twaweza.js
+++ js/twaweza.js
@@ -1,4 +1,12 @@
 function showHideBar(x) {
     x.classList.toggle("change");
-    
+    openNav()
+}
+
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
 }
